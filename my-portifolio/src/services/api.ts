b4d92a5aa737c5200e80b931e0c393604c702aa1
Amanda de Oliveira Pinheiro <amandajauregui.dev@gmail.com>
2024-05-23 20:04:12 -0300

    mudança do dominio base do axios

--- my-portifolio/src/services/api.ts
+++ my-portifolio/src/services/api.ts
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import axios from 'axios'
 
 export default axios.create({
-        baseURL: process.env.DEPLOY_URL_API || "http://localhost:3003/pt",
+        baseURL: process.env.DEPLOY_URL_API,
         timeout: 10000,
         headers: {
         'Content-Type': 'application/json'
