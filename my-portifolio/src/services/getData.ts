48572cd65d94f760016a5cfe63d47b63e6355e9a
Amanda de Oliveira Pinheiro <amandajauregui.dev@gmail.com>
2024-05-23 20:11:20 -0300

    ajuste no dominio do axios

--- my-portifolio/src/services/getData.ts
+++ my-portifolio/src/services/getData.ts
@@ -1,6 +1,6 @@
 import api from '../services/api'
 
 export default async function getData() {
-    const response = await api.get('/')
+    const response = await api.get('/pt')
     return response.data
 }
