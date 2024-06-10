7142bbe2e6afb9cd9d4edeb49943e376ea714b93
Amanda de Oliveira Pinheiro <amandajauregui.dev@gmail.com>
2024-06-10 19:48:26 -0300

    sidebar responsiva

--- my-portifolio/src/components/sideBarInfos/Aside.tsx
+++ my-portifolio/src/components/sideBarInfos/Aside.tsx
@@ -31,9 +31,9 @@ export default function Aside() {
 
 
     return (
-        <aside className="lg:w-max h-full w-full flex flex-col self-start items-center sm:col-start-1 lg:col-end-3 sm:col-end-2 col-start-1 col-end-13 sm:row-span-12 row-start-1 row-end-5 animate-fade-right animate-duration-300 animate-ease-in">
+        <aside className="lg:w-max sm:h-full h-fit w-full flex flex-col self-start items-center sm:col-start-1 lg:col-end-3 sm:col-end-2 col-start-1 col-end-13 sm:row-span-12 row-start-1 row-end-3 animate-fade-right animate-duration-300 animate-ease-in">
             <Picture/>
-        <section className="mt-14 lg:px-6 px-4 pt-16 pb-10 flex flex-col items-center lg:justify-between justify-evenly w-fit h-full bg-not-black rounded-xl gap-6">
+        <section className="mt-14 lg:px-6 px-4 pt-16 pb-10 flex flex-col items-center lg:justify-between justify-evenly sm:w-fit w-full h-full bg-not-black rounded-xl gap-6">
             <div>
             <h1 className="text-center font-realce lg:text-5xl text-3xl lg:mt-24 mt-16">Amanda Oliveira</h1>
             <h2 className="text-center font-semibold lg:text-xl text-lg">Desenvolvedora BackEnd</h2>
