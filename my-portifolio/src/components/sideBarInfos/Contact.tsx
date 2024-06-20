c07916eb67ddd1525d409c113179f6241fe8fb87
Amanda de Oliveira Pinheiro <amandajauregui.dev@gmail.com>
2024-06-20 09:51:25 -0300

    hotfix: ajuste do tamanho do ícone da aba de contato

--- my-portifolio/src/components/sideBarInfos/Contact.tsx
+++ my-portifolio/src/components/sideBarInfos/Contact.tsx
@@ -23,7 +23,7 @@ const icons = [
 
 export default function Contact() {
     return(
-        <section className="bg-not-white/10 lg:p-5 p-4 lg:max-w-96 w-[240px] h-max rounded-xl sm:flex sm:flex-col hidden">
+        <section className="bg-not-white/10 lg:p-5 p-2 lg:max-w-96 w-[240px] h-max rounded-xl sm:flex sm:flex-col hidden">
             {
                 icons.map((icon) =>(
                     <Link 
@@ -32,13 +32,12 @@ export default function Contact() {
                     rel="noopener noreferrer"
                     key={icon.name}
                     >
-                    
                     <div className="flex gap-3 mb-4 justify-start items-center">
-                        <div className="bg-not-black rounded-xl lg:w-14 lg:h-14 w-11 h-11 flex justify-center items-center">
-                            <img className="lg:w-9 lg:h-9 w-7 h-7" src={icon.path} alt={`${icon.name} icone`} /></div>
+                        <div className="bg-not-black rounded-xl lg:w-13 lg:h-13 w-11 h-11 flex justify-center items-center">
+                            <img className="lg:w-8 lg:h-8 w-7 h-7" src={icon.path} alt={`${icon.name} icone`} /></div>
                         <div>
                             <p className="font-realce lg:text-lg text-md">{icon.name}</p>
-                            <p className="font-realce lg:text-md text-sm">{icon.content}</p>
+                            <p className="font-realce lg:text-md text-[12px]">{icon.content}</p>
                         </div>
                     </div>
                     </Link>
